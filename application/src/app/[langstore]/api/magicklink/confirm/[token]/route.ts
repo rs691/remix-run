ad8e966a44cf45bdcf8af26b886a0784f3ccbaa9
rs691 <robstewart@my365.bellevue.edu>
2024-05-29 20:38:50 -0500

    Adding initial Next-ecommerce files

---
+++ application/src/app/[langstore]/api/magicklink/confirm/[token]/route.ts
@@ -0,0 +1,30 @@
+import { getContext } from '~/use-cases/http/utils';
+import handleMagickLink from '~/use-cases/user/handleMagickLink';
+
+import { serialize } from 'cookie';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request, { params }: { params: { token: string } }) {
+    const requestContext = getContext(request);
+
+    const { redirectUrl, cookie } = await handleMagickLink(
+        requestContext.baseUrl,
+        requestContext,
+
+        params.token,
+    );
+
+    return new Response(null, {
+        status: 302,
+        headers: {
+            Location: redirectUrl,
+            'Set-Cookie': serialize('authentication', JSON.stringify(cookie), {
+                path: '/',
+                httpOnly: true,
+                secure: true,
+                sameSite: 'lax',
+                maxAge: 604_800,
+            }),
+        },
+    });
+}
